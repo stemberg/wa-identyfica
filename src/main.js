19561b98e3f4b4da0af4354106a8fac939d59b55
Stemberg Figueiredo <95388236+stemberg@users.noreply.github.com>
2023-05-29 15:13:19 -0300

    Ajuste em import

--- src/main.js
+++ src/main.js
@@ -2,6 +2,7 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import 'vue-toastification/dist/index.css';
 import 'bulma/css/bulma.css'
 import Toast from "vue-toastification";
 import VueTheMask from "vue-the-mask";
